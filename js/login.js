1721801b35c6886803e5566a77a5665f6a277722
SheilaMart <sheilamartinez372@gmail.com>
2025-08-19 21:04:40 -0300

    Agregamos el código JS y formulario en html

---
+++ js/login.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+const form = document.getElementById ('loginForm');
+const usuario = document.getElementById ('usuario');
+const password = document.getElementById('password');
+
+if (usuario==="" || password===""){
+    alert ("Inválido");
+}
+});
