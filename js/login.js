33a401726018b8cf1a993789c4fc946332db80ca
Loana Leguizamon <loanaleguizamon1@gmail.com>
2025-08-21 08:58:46 -0300

    cambios

--- js/login.js
+++ js/login.js
@@ -3,7 +3,16 @@ const form = document.getElementById ('loginForm');
 const usuario = document.getElementById ('usuario');
 const password = document.getElementById('password');
 
-if (usuario==="" || password===""){
+if (usuario.value==="" || password.value===""){
     alert ("Inválido");
 }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const boton = document.getElementById('boton');
+    
+    boton.addEventListener("click", (e) => {
+        e.preventDefault();
+        window.location.href = "http://127.0.0.1:5500/workspace-inicial-grupo5/";
+    });
 });
