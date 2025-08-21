06a89ace67b36294376a7344c008fb59dff7fc07
Kevin Lopetegui <you@example.com>
2025-08-21 18:56:22 -0300

    Fondo

--- js/login.js
+++ js/login.js
@@ -1,9 +1,12 @@
+// declaramos las constantes para trabajar mejor //
 document.addEventListener("DOMContentLoaded", () => {
 const form = document.getElementById ('loginForm');
 const usuario = document.getElementById ('usuario');
 const password = document.getElementById('password');
 const boton = document.querySelector("form button[type='button']");
 
+// Código para que al darle al botón nos redireccione a la portada 
+// y salte una alerta si los campos están vacíos //
   boton.addEventListener("click", function () {
     if (usuario.value === "" || password.value === "") {
       alert("Inválido");
