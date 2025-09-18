320e4a13c0c007d66cc1e277811cc455246fcd2c
SheilaMart <sheilamartinez372@gmail.com>
2025-09-18 09:20:47 -0300

    Pauta desafiate entrega 3

--- js/products.js
+++ js/products.js
@@ -25,4 +25,53 @@
         console.error("Error:", error);
       });
 
-  
+//Pauta desafíate.
+var productos = [];
+
+function mostrarProductos(lista){
+  const contenedor = document.getElementById("lista-productos");
+  let contenido = "";
+
+  if (lista.length === 0){
+    contenedor.innerHTML = "<p>Sin resultados.</p>";
+    return;
+  }
+
+
+  for (let i=0; i < lista.length; i++){
+    contenido= contenido + `
+    <div class="card">
+    <img src="${lista[i].image}" class="card-img">
+    <div class="card-info">
+    <h3 class="card-title">${lista[i].name}</h3>
+    <p class="card-description">${lista[i].description}</p>
+    <p class="card-price">${lista[i].currency} ${lista[i].cost}</p>
+    <p class="card-sold">Cantidad de vendidos: ${lista[i].soldCount}<br></p>
+    </div>
+    </div>
+    `;
+  }
+  contenedor.innerHTML = contenido;
+}
+
+getJSONData(PRODUCTS_URL + "101" + EXT_TYPE).then(function(resultado){
+  if (resultado.status === "ok") {
+    productos = resultado.data.products;
+    mostrarProductos(productos);
+  }
+});
+
+document.getElementById("buscador").addEventListener("input", (event) => {
+  let busqueda = event.target.value.toLowerCase();
+  let filtrados= [];
+
+  for (let i=0; i < productos.length; i++){
+    let nombreProducto= productos[i].name.toLowerCase();
+    let descripcionProducto= productos[i].description.toLowerCase();
+
+    if (nombreProducto.indexOf(busqueda) !==-1 || descripcionProducto.indexOf(busqueda) !==-1){
+      filtrados.push(productos[i]);
+    }
+  }
+mostrarProductos(filtrados);
+});
